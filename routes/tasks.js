71bf0860800b1e54916b819d728cb4477f6fa811
Filipe <filipe.viana@ufv.br>
2021-11-18 00:07:55 -0300

    apply middleware on routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const route = express();
-const TaskController = require("../controllers/TaskController")
+const TaskController = require("../controllers/TaskController");
+const { body } = require("express-validator");
+const verify = require("../services/TaskMiddleware");
 
-route.get("/", TaskController.listAll)
-    .get("/:taskId", TaskController.listOne)
-    .post("/", TaskController.create)
-    .put("/:taskId", TaskController.update)
-    .delete("/:taskId", TaskController.delete)
+route
+	.get("/", TaskController.listAll)
+	.get("/:taskId", TaskController.listOne)
+	.post(
+		"/",
+		body("name").isString().withMessage("Name need be string"),
+		body("description").isString().withMessage("Description need be string"),
+		body("file").isString().withMessage("File need be string"),
+		verify,
+		TaskController.create
+	)
+	.put(
+		"/:taskId",
+		body("name").isString().withMessage("Name need be string"),
+		body("description").isString().withMessage("Description need be string"),
+		body("file").isString().withMessage("File need be string"),
+		verify,
+		TaskController.update
+	)
+	.delete("/:taskId", TaskController.delete);
 
 module.exports = route;
