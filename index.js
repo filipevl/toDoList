7ae1401ebef5e94eabba2daa9825d9ac046e54c3
Filipe <filipe.viana@ufv.br>
2021-11-17 22:52:25 -0300

    Add body-parser Add routes parent Use body-parser Use routes parent

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes")
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "http://localhost";
+app.use(routes)
+
 
 app.listen(port, () => {
 	console.log(`To do list API listening at ${host}:${port}`);
