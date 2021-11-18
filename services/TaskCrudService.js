9b116561ddcd04d8bc0e969a080a3b3c05482d8a
Filipe <filipe.viana@ufv.br>
2021-11-17 23:53:28 -0300

    Add Task model

--- services/TaskCrudService.js
+++ services/TaskCrudService.js
@@ -1,52 +1,54 @@
+const { Task } = require("../models");
+
 class TaskCrudService {
-	select = (id = null) => {
-		const tasks = [
-			{
-				id: 0,
-				name: "Estudar para prova de matemática",
-				description: "Procurar no capítulo 3 do livro exercícios e resolver",
-			},
-			{
-				id: 1,
-				name: "Estudar para prova de português",
-				description: "Procurar no capítulo 4 do livro exercícios e resolver",
-			},
-			{
-				id: 2,
-				name: "Estudar para prova de história",
-				description: "Procurar no capítulo 1 do livro exercícios e resolver",
-			},
-			{
-				id: 3,
-				name: "Estudar para prova de geografia",
-				description: "Procurar no capítulo 9 do livro exercícios e resolver",
-			},
-		];
-		return tasks;
+	select = async (id = null) => {
+		if (id) {
+			try {
+				let tasks = await Task.findByPk(id);
+				return { status: 200, data: tasks };
+			} catch (e) {
+				return { status: 500, error: e };
+			}
+		} else {
+			try {
+				let tasks = await Task.findAll();
+				return { status: 200, data: tasks };
+			} catch (e) {
+				return { status: 500, error: e };
+			}
+		}
 	};
-	create = (name, description = null, file_path = null) => {
-		const task = {
-			id: 0,
-			name: "Estudar para prova de matemática",
-			description: "Procurar no capítulo 3 do livro exercícios e resolver",
-		};
-		return task;
+	create = async (name, description = null, file_path = null) => {
+		try {
+			let task = await Task.create({ name, description, file_path });
+			return { status: 202, data: task };
+		} catch (e) {
+			return { status: 406, error: e };
+		}
 	};
-	update = (taskId, name = null, description = null, file_path = null) => {
-		const task = {
-			id: 0,
-			name: "Estudar para prova de matemática",
-			description: "Procurar no capítulo 3 do livro exercícios e resolver",
-		};
-		return task;
+	update = async (taskId, name = null, description = null, file_path = null) => {
+		try {
+			if (taskId === undefined) throw new Error("taskId can't undefined");
+			let task = await Task.findByPk(taskId);
+			if (name) task.name = name;
+			if (description) task.description = description;
+			if (file_path) task.file_path = file_path;
+			task.save();
+			return { status: 202, data: task };
+		} catch (e) {
+			return { status: 406, error: e };
+		}
 	};
-	delete = (id) => {
-		const task = {
-			id: 0,
-			name: "Estudar para prova de matemática",
-			description: "Procurar no capítulo 3 do livro exercícios e resolver",
-		};
-		return task;
+	delete = async (taskId) => {
+		try {
+			if (taskId === undefined) throw new Error("taskId can't undefined");
+			let task = await Task.findByPk(taskId),
+				taskcp = task.dataValues;
+			task.destroy();
+			return { status: 202, data: taskcp };
+		} catch (e) {
+			return { status: 406, error: e };
+		}
 	};
 }
 module.exports = new TaskCrudService();
